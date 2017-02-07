527a1bb09239fabbbbbe3b01c0032c0268e038e5
WON SEOB SEO <won.seob.seo@zalando.fi>
2017-02-07 15:48:37 +0200

    Add simple mutations to increment and decrement

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -27,6 +27,24 @@
   [{:keys [state] :as env} key params]
   {:value (get-people state key)})
 
+(defmulti mutate om/dispatch)
+
+(defmethod mutate 'points/increment
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+            [:person/by-name name :points]
+            inc))})
+
+(defmethod mutate 'points/decrement
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+            [:person/by-name name :points]
+            #(let [n (dec %)] (if (neg? n) 0 n))))})
+
 (defui Person
   static om/Ident
   (ident [this {:keys [name]}]
